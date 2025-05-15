59b9d3a3de7bff42e375b4b16e21d82a4ac3f7da
Dark Lord <adamsuper148@gmail.com>
2025-05-15 16:05:25 +0300

    connection to db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URI
+).then(() => console.log("db is connected")).catch((err) => console.log('Error connection db', err))
 
 const app = express();
 
