398b5dc9c0021ef9fe894fd7d3b203ef324cfc60
Dark Lord <adamsuper148@gmail.com>
2025-05-21 22:57:36 +0300

    Profile Page UI

---
+++ client/src/components/DashProfile.jsx
@@ -0,0 +1,34 @@
+import { Button, TextInput } from 'flowbite-react';
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const DashProfile = () => {
+    const { currentUser } = useSelector(state => state.user);
+    return (
+        <div className='max-w-lg mx-auto p-3 w-full'>
+            <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+            <form className='flex flex-col gap-4'>
+                <div className="w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden">
+                    <img
+                        src={currentUser.profilePicture}
+                        alt="profile_picture"
+                        className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'
+                    />
+                </div>
+
+                <TextInput className='w-full' type='text' id='username' placeholder='username' defaultValue={currentUser.username} autoComplete='username' />
+                <TextInput className='w-full' type='email' id='email' placeholder='email' defaultValue={currentUser.email} autoComplete='email' />
+                <TextInput className='w-full' type='password' id='password' placeholder='password' autoComplete='current-password' />
+                
+                <Button type='submit' className='bg-gradient-to-r from-purple-500 to-blue-500 cursor-pointer'>Update</Button>
+            </form>
+
+            <div className="text-red-500 flex justify-between items-center mt-5">
+                <div className="cursor-pointer">Delete account</div>
+                <div className="cursor-pointer">Sign Out</div>
+            </div>
+        </div>
+    )
+}
+
+export default DashProfile
---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from 'flowbite-react'
+import { HiUser, HiArrowSmRight } from 'react-icons/hi'
+import { Link, useLocation } from 'react-router-dom';
+
+const DashSidebar = () => {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+
+        if (tabFromUrl) setTab(tabFromUrl);
+    }, [location.search]);
+
+    return (
+        <Sidebar className='w-full md:w-56'>
+            <SidebarItems>
+                <SidebarItemGroup >
+                    <SidebarItem href='/dashboard?tab=profile' active={tab === 'profile'} icon={HiUser} label='User' labelColor='red'>Profile</SidebarItem>
+                    <SidebarItem icon={HiArrowSmRight} className='cursor-pointer'>Sign Out</SidebarItem>
+                </SidebarItemGroup>
+            </SidebarItems>
+        </Sidebar>
+    )
+}
+
+export default DashSidebar
