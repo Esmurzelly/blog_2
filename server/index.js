618a3f54c916686a37f2cb199f639869010cd437
Dark Lord <adamsuper148@gmail.com>
2025-05-23 18:16:33 +0300

    add post API

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
+import postRoutes from './routes/post.route.js'
 import { errorHandler } from './middleware/errorHandler.js';
 import cookiePaser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
@@ -28,6 +29,7 @@ app.use("/static", express.static(path.join(__dirname, "static")));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 app.use(errorHandler)
 
