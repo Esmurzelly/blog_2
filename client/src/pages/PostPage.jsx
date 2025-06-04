a8c2d78e51be3b8040b4d29ee67341a54cbebe5b
Dark Lord <adamsuper148@gmail.com>
2025-06-04 16:21:53 +0300

    recent article section

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -4,12 +4,14 @@ import { Link, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify';
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 
 const PostPage = () => {
     const { postSlug } = useParams();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [post, setPost] = useState(null);
+    const [recentPosts, setRecentPosts] = useState(null);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -35,6 +37,23 @@ const PostPage = () => {
         fetchPost();
     }, [postSlug]);
 
+    useEffect(() => {
+        try {
+            const fetchRecentPosts = async () => {
+                const res = await fetch(`/api/post/getposts?limit=3`);
+                const data = await res.json();
+
+                if (res.ok) {
+                    setRecentPosts(data.posts);
+                }
+            }
+            fetchRecentPosts();
+        } catch (error) {
+            console.log(error.message);
+            toast.error(error.message);
+        }
+    }, []);
+
     if (loading) {
         return (
             <div className="flex items-center justify-center min-h-screen">
@@ -44,7 +63,7 @@ const PostPage = () => {
     }
 
     return (
-        <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+        <main className='p-3 flex flex-col w-full mx-auto min-h-screen'> {/* max-w-6xl mx */}
             <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl font-semibold'>{post && post.title}</h1>
 
             <Link to={`/search?category=${post?.category}`} className='self-center mt-5'>
@@ -69,6 +88,14 @@ const PostPage = () => {
             </div>
 
             <CommentSection postId={post._id} />
+
+            <div className='flex flex-col justify-center items-center mb-5'>
+                <h1 className='text-xl mt-5'>Recent articles</h1>
+                <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+                    {recentPosts &&
+                        recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+                </div>
+            </div>
         </main>
     )
 }
